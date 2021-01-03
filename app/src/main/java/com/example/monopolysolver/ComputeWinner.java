b26380627f757d768db8270b24b685e33706ddf4
Antonio Gutierrez <augutierrez@dons.usfca.edu>
2021-01-02 16:16:11 -0800

    Added monopoly logic to run in a different thread.  The UI no longer freezes and shows "Loading..." while the winner is being computed.  I also added flags to the compute again button that deletes the history stack.

--- app/src/main/java/com/example/monopolysolver/ComputeWinner.java
+++ app/src/main/java/com/example/monopolysolver/ComputeWinner.java
@@ -29,11 +29,8 @@ public class ComputeWinner extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compute_winner);
 
-
-
         winText = findViewById(R.id.winner_text_computer_winner);
-        String winner = "loading...";
-        winText.setText("Winner: Player " + winner);
+        winText.setText("Winner: Loading...");
 
         roundText = findViewById(R.id.rounds_text_compute_winner);
         roundText.setText("Loading...");
@@ -44,6 +41,7 @@ public class ComputeWinner extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ComputeWinner.this, ComputeWinner.class);
+                //these flags clear the history of the activities
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
             }
@@ -53,34 +51,11 @@ public class ComputeWinner extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-
-        //+1 to match user index
-//        ArrayList<Long> bal = (ArrayList<Long>)MonopolySolver.balances.clone();
-//        ArrayList<Integer>pos = (ArrayList<Integer>)MonopolySolver.positions.clone();
-//        ArrayList<MonopolyCard> d = (ArrayList<MonopolyCard>)MonopolySolver.deck.clone();
-        //String n = new RunInBackgroun
-
-        /*Thread thread = new Thread(s);
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-
-//        Handler handler = new Handler();
         Runnable solver = new Solver();
         Thread thread = new Thread(solver);
+        //set priority to max to speed up the process
         thread.setPriority(Thread.MAX_PRIORITY);
         thread.start();
-        //winText.post(solver);
-        //this.runOnUiThread(solver);
-
-        /*winner = MonopolyLogic.solveGame(bal, pos, d, 0) + 1;
-
-        winText.setText("Winner: Player " + winner);
-        roundText.setText("Rounds:" + MonopolyLogic.round);*/
-
     }
 
     public class Solver implements Runnable {
